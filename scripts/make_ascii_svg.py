5f824c70ac8f14a328ca9407792c218117efd192
souvik mndal <souvkmndal@gmail.com>
2026-09-08 02:50:35 +0530

    github profile

--- scripts/make_ascii_svg.py
+++ scripts/make_ascii_svg.py
@@ -24,7 +24,7 @@ SRC = sys.argv[1] if len(sys.argv) > 1 else os.path.join(HERE, "..", "source-pre
 OUT = sys.argv[2] if len(sys.argv) > 2 else os.path.join(HERE, "..", "avi-ascii.svg")
 
 COLS = 100
-ROWS = 63
+ROWS = 67
 CELL_W = 8
 CELL_H = 15
 RAMP = " .`:-=+*cs#%@"  # bright(sparse) -> dark(dense); leading space clears bg
